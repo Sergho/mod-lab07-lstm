7bf82970652bdde17ca2710b99fb3fe08afd97dd
Sergho <sergche04@gmail.com>
2025-05-23 18:30:22 +0300

    feat: words generating

--- src/lstm.py
+++ src/lstm.py
@@ -1,3 +1,39 @@
+import random
+import numpy as np
+
+from keras.models import Sequential
+from keras.layers import Dense, Activation
+from keras.layers import LSTM
+ 
+from keras.optimizers import RMSprop
+
+def sample_index(preds, temperature = 1.0):
+    preds = np.asarray(preds).astype('float64')
+    preds = np.log(preds) / temperature
+    expPreds = np.exp(preds)
+    preds = expPreds / np.sum(expPreds)
+    probas = np.random.multinomial(1, preds, 1)
+    return np.argmax(probas)
+
+def generate_text(length, diversity):
+  startIndex = random.randint(0, len(words) - maxChainLength - 1)
+  generated = ''
+  sentence = words[startIndex: startIndex + maxChainLength]
+  generated = sentence.copy()
+  for i in range(length):
+    xPred = np.zeros((1, maxChainLength, len(vocabulary)))
+    for t, word in enumerate(sentence):
+      xPred[0, t, wordToIndices[word]] = 1.
+
+    preds = model.predict(xPred, verbose = 0)[0]
+    nextIndex = sample_index(preds, diversity)
+    nextWord = indicesToWords[nextIndex]
+
+    generated.append(nextWord)
+    sentence.append(nextWord)
+    sentence = sentence[1:]
+  return ' '.join(generated)
+ 
 with open('src/input.txt', 'r') as file:
   words = file.read().lower().split()
 
@@ -14,4 +50,21 @@ for i in range(0, len(words) - maxChainLength, 1):
   sentences.append(words[i:i+maxChainLength])
   nextWords.append(words[i+maxChainLength])
 
-print(sentences)
+X = np.zeros((len(sentences), maxChainLength, len(vocabulary)), dtype=np.bool)
+Y = np.zeros((len(sentences), len(vocabulary)), dtype=np.bool)
+
+for i, sentence in enumerate(sentences):
+  for t, word in enumerate(sentence):
+    X[i, t, wordToIndices[word]] = 1
+  Y[i, wordToIndices[nextWords[i]]] = 1
+
+model = Sequential()
+model.add(LSTM(128, input_shape=(maxChainLength, len(vocabulary))))
+model.add(Dense(len(vocabulary)))
+model.add(Activation('softmax'))
+optimizer = RMSprop(learning_rate=0.01)
+model.compile(loss='categorical_crossentropy', optimizer=optimizer)
+
+model.fit(X, Y, batch_size=128, epochs=50)
+
+print(generate_text(500, 0.2))
